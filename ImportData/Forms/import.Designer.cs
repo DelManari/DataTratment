b2021818fead47602ba1c5319bdcdb38dba8f23e
Mtir Manari <mtir158@gmail.com>
2018-10-23 19:23:08 +0100

    init commit

---
+++ ImportData/Forms/import.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ImportData
+{
+    partial class importDataForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtType = new System.Windows.Forms.ComboBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtdatabaseName = new System.Windows.Forms.TextBox();
+            this.databaseTables = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data Source Type";
+            // 
+            // txtType
+            // 
+            this.txtType.FormattingEnabled = true;
+            this.txtType.Items.AddRange(new object[] {
+            "DataBase File",
+            "Excel File",
+            "CSV File"});
+            this.txtType.Location = new System.Drawing.Point(112, 28);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(121, 21);
+            this.txtType.TabIndex = 1;
+            this.txtType.Text = "--Select--";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(15, 95);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(599, 194);
+            this.dataGridView.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtdatabaseName
+            // 
+            this.txtdatabaseName.Location = new System.Drawing.Point(382, 12);
+            this.txtdatabaseName.Name = "txtdatabaseName";
+            this.txtdatabaseName.Size = new System.Drawing.Size(100, 20);
+            this.txtdatabaseName.TabIndex = 6;
+            // 
+            // databaseTables
+            // 
+            this.databaseTables.FormattingEnabled = true;
+            this.databaseTables.Location = new System.Drawing.Point(382, 39);
+            this.databaseTables.Name = "databaseTables";
+            this.databaseTables.Size = new System.Drawing.Size(121, 21);
+            this.databaseTables.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(413, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "GetTables";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(502, 296);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "CSV";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(412, 296);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "EXEL";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(331, 295);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "SQL DataBase";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(285, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "DataBase Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(285, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Tabels Name :";
+            // 
+            // importDataForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 380);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.databaseTables);
+            this.Controls.Add(this.txtdatabaseName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.txtType);
+            this.Controls.Add(this.label1);
+            this.Name = "importDataForm";
+            this.Text = "Import Data";
+            this.Load += new System.EventHandler(this.importDataForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox txtType;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtdatabaseName;
+        private System.Windows.Forms.ComboBox databaseTables;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ImportData/Forms/import.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using DataTable = System.Data.DataTable;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ImportData
+{
+    public partial class importDataForm : Form
+    {
+        public importDataForm()
+        {
+            InitializeComponent();
+        }
+
+        private void importDataForm_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string strFilePath;
+        public DataTable GetCsv()
+        {
+            DataTable dt = new DataTable();
+            using (StreamReader sr = new StreamReader(strFilePath))
+            {
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    dt.Columns.Add(header);
+                }
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(',');
+                    DataRow dr = dt.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                    }
+                    dt.Rows.Add(dr);
+                }
+                return dt;
+
+            }
+        }
+
+        private void txtFilePath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFilePath_MouseUp(object sender, MouseEventArgs e)
+        {
+        }
+        private void GetDataFromExcel()
+        {
+            Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(fname);
+            Microsoft.Office.Interop.Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Microsoft.Office.Interop.Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            // dt.Column = colCount;  
+            dataGridView.ColumnCount = colCount;
+            dataGridView.RowCount = rowCount;
+
+            for (int i = 1; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= colCount; j++)
+                {
+
+
+                    //write the value to the Grid  
+
+
+                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                    {
+                        dataGridView.Rows[i - 1].Cells[j - 1].Value = xlRange.Cells[i, j].Value2.ToString();
+                    }
+                    // Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");  
+
+                    //add useful things here!     
+                }
+            }
+
+            //cleanup  
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:  
+            //  never use two dots, all COM objects must be referenced and released individually  
+            //  ex: [somthing].[something].[something] is bad  
+
+            //release com objects to fully kill excel process from running in the background  
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release  
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release  
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+
+
+        }
+       static string databasename = "";
+        string fname = "";
+        string databaseTable;
+       // ArticleV2
+ 
+        public IList<string> ListTables()
+        {
+            string myConnecting = @"Server= DESKTOP-5801JMQ\SQLEXPRESS01; Database= " + databasename + "; Integrated Security=True;";
+
+            SqlConnection con = new SqlConnection(myConnecting);
+            con.Open();
+            List<string> tables = new List<string>();
+            DataTable dt = con.GetSchema("Tables");
+            foreach (DataRow row in dt.Rows)
+            {
+                string tablename = (string)row[2];
+                tables.Add(tablename);
+            }
+            con.Close();
+            return tables;
+        }
+        
+        public DataTable getDataBaseData()
+        {
+            databaseTable = databaseTables.SelectedItem.ToString();
+
+
+            var table = new DataTable();
+            string myConnecting = @"Server= DESKTOP-5801JMQ\SQLEXPRESS01; Database= " + databasename + "; Integrated Security=True;";
+
+            using (var da = new SqlDataAdapter("SELECT * FROM "+databaseTable, myConnecting))
+            {
+                da.Fill(table);
+            }
+            return table;
+
+        }
+        public void getDataBaseTables()
+        {
+            databaseTables.DataSource = ListTables();
+        }
+        private void button1_Click(object sender, EventArgs e)
+            
+        {
+          
+            if (txtType.SelectedItem.ToString() == "Excel File")
+            {
+                OpenFileDialog fdlg = new OpenFileDialog();
+                fdlg.Title = "Excel File Dialog";
+                fdlg.InitialDirectory = @"E:\";
+                fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+                fdlg.FilterIndex = 2;
+                fdlg.RestoreDirectory = true;
+                if (fdlg.ShowDialog() == DialogResult.OK)
+                {
+                    fname = fdlg.FileName;
+                    strFilePath = fdlg.FileName;
+
+                }
+                dataGridView.DataSource = null;
+
+                GetDataFromExcel();
+
+            }
+            if (txtType.SelectedItem.ToString() == "CSV File")
+            {
+                OpenFileDialog fdlg = new OpenFileDialog();
+                fdlg.Title = "Excel File Dialog";
+                fdlg.InitialDirectory = @"c:\";
+                fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+                fdlg.FilterIndex = 2;
+                fdlg.RestoreDirectory = true;
+                if (fdlg.ShowDialog() == DialogResult.OK)
+                {
+                    fname = fdlg.FileName;
+                    strFilePath = fdlg.FileName;
+
+                }
+                dataGridView.DataSource = null;
+
+                dataGridView.DataSource = GetCsv();
+            }
+            if (txtType.SelectedItem.ToString() == "DataBase File")
+            {
+                dataGridView.DataSource = null;
+
+                dataGridView.DataSource = getDataBaseData();
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            databasename = txtdatabaseName.Text;
+            getDataBaseTables();
+            getDataBaseData();
+        }
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.FolderBrowserDialog fbd;
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //Build the CSV file data as a Comma separated string.
+                string csv = string.Empty;
+
+                //Add the Header row for CSV file.
+                foreach (DataGridViewColumn column in dataGridView.Columns)
+                {
+                    csv += column.HeaderText + ',';
+                }
+                //Add new line.
+                csv += "\r\n";
+
+                //Adding the Rows
+
+                foreach (DataGridViewRow row in dataGridView.Rows)
+                {
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        if (cell.Value != null)
+                        {
+                            //Add the Data rows.
+                            csv += cell.Value.ToString().TrimEnd(',').Replace(",", ";") + ',';
+                        }
+                        // break;
+                    }
+                    //Add new line.
+                    csv += "\r\n";
+                }
+
+                //Exporting to CSV.
+                string folderPath = string.Empty;
+
+                using (FolderBrowserDialog fdb = new FolderBrowserDialog())
+                {
+                    if (fdb.ShowDialog() == DialogResult.OK)
+                    {
+                        folderPath = fdb.SelectedPath + "\\";
+                        MessageBox.Show(folderPath);
+                    }
+                }
+        
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                File.WriteAllText(folderPath + "Exported.csv", csv);
+                MessageBox.Show("done bb");
+            }
+            catch
+            {
+                MessageBox.Show("errror");
+            }
+
+
+        }
+
+        private void copyAlltoClipboard()
+        {
+            dataGridView.SelectAll();
+            DataObject dataObj = dataGridView.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occurred while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel Documents (*.xls)|*.xls";
+            sfd.FileName = "Export.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                // Copy DataGridView results to clipboard
+                copyAlltoClipboard();
+
+                object misValue = System.Reflection.Missing.Value;
+                Excel.Application xlexcel = new Excel.Application();
+
+                xlexcel.DisplayAlerts = false; // Without this you will get two confirm overwrite prompts
+                Excel.Workbook xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                // Format column D as text before pasting results, this was required for my data
+                Excel.Range rng = xlWorkSheet.get_Range("D:D").Cells;
+                rng.NumberFormat = "@";
+
+                // Paste clipboard results to worksheet range
+                Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                CR.Select();
+                xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+
+                // For some reason column A is always blank in the worksheet. ¯\_(ツ)_/¯
+                // Delete blank column A and select cell A1
+                Excel.Range delRng = xlWorkSheet.get_Range("A:A").Cells;
+                delRng.Delete(Type.Missing);
+                xlWorkSheet.get_Range("A1").Select();
+
+                // Save the excel file under the captured location from the SaveFileDialog
+                xlWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlexcel.DisplayAlerts = true;
+                xlWorkBook.Close(true, misValue, misValue);
+                xlexcel.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlexcel);
+
+                // Clear Clipboard and DataGridView selection
+                Clipboard.Clear();
+                dataGridView.ClearSelection();
+
+                // Open the newly saved excel file
+                if (File.Exists(sfd.FileName))
+                    System.Diagnostics.Process.Start(sfd.FileName);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string StrQuery;
+            databasename = txtdatabaseName.Text;
+            string ConnString = @"Server= DESKTOP-5801JMQ\SQLEXPRESS01; Database= " + databasename + "; Integrated Security=True;";
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnString))
+                {
+                    using (SqlCommand comm = new SqlCommand())
+                    {
+                        comm.Connection = conn;
+                        conn.Open();
+                        for (int i = 0; i < dataGridView.Rows.Count; i++)
+                        {
+                            StrQuery = @"INSERT INTO "+ databaseTables.SelectedValue+ " VALUES ("
+                                + dataGridView.Rows[i].Cells[1].Value + ", "
+                                + dataGridView.Rows[i].Cells[2].Value + ", "
+                                + dataGridView.Rows[i].Cells[3].Value + ", "
+                                + dataGridView.Rows[i].Cells[4].Value + ", "
+                                + dataGridView.Rows[i].Cells[5].Value + ", "
+                                + dataGridView.Rows[i].Cells[6].Value + ");";
+                            comm.CommandText = StrQuery;
+                            comm.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+    }
+}
+
